00d163e91a3dc56e4804fc1d32c18290f19be00a
SamaadTurner <samaad.turner@gmail.com>
2023-09-19 23:33:16 -0700

    great start

---
+++ src/middleware/validator.js
@@ -0,0 +1,13 @@
+
+const validator = (req, res, next) => {
+    const name = req.query.name;
+  
+    if (!name) {
+      return res.status(400).json({ error: 'Name parameter is missing' });
+    }
+  
+    // If the name parameter is present, continue to the next middleware or route
+    next();
+  }
+  
+  module.exports = validator;
